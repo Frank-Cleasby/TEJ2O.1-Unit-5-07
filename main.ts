d7f3cbcde0806fda55ae05ab4d12992773d5ec24
Frank-Cleasby <frank.cleasby@stu.ocsb.ca>
2025-10-22 12:50:45 -0400

    works

--- main.ts
+++ main.ts
@@ -1,8 +1,33 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Frank
+ * Created on: Oct 2025
+ * This program moves a motor
 */
 
-basic.showString('Hello, World!')
+// variables
+const servoNumberOne = robotbit.Servos.S1
+
+// start
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// move 0 degrees
+input.onButtonPressed(Button.A, function() {
+    robotbit.Servo(servoNumberOne, 0)
+    basic.showNumber(0)
+    basic.clearScreen()
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+// move 180 degrees
+input.onButtonPressed(Button.B, function() {
+    robotbit.Servo(servoNumberOne, 180)
+    basic.showNumber(180)
+    basic.clearScreen()
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
